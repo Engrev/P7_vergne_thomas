48c7a9ccb77867c41d7606ba045e2b3b860f3efb
Thomas Vergne <tvergne83@gmail.com>
2020-09-17 18:45:38 +0200

    Create IndexController.php

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->redirect($_ENV['APP_DOMAIN_NAME_ENTITIES_LINKS'], 301);
+    }
+}
